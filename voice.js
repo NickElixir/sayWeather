5a1bc26f226cc5c92a4613439bd8afb1cb6c1d48
Windows 7 <free_x@vfemail.net>
2017-01-03 16:03:44 +0300

    Реализован Voice API
    В тестовом файле все работает.

--- voice.js
+++ voice.js
@@ -1,1 +1,10 @@
 var apiKey = "4f2153e7bff44d83ab8436be4500b476";
+VoiceRSS.speech({
+            key: '4f2153e7bff44d83ab8436be4500b476',
+            src: 'Привет, Мир!',
+            hl: 'en-us',
+            r: 0, 
+            c: 'mp3',
+            f: '44khz_16bit_stereo',
+            ssml: false
+        });
