087fa6edb1a29b153f75189589accef1c5b809cf
Niсk_Elixir <free_x@vfemail.net>
2017-02-23 19:39:42 +0300

    Timers for speech
    Добавлен новый класс таймеров для озвучивания текста.

--- user.js
+++ user.js
@@ -11,6 +11,7 @@ class SayWeatherUserData extends UserData{
         this.language = language;
         this.voiceApiKey = "4f2153e7bff44d83ab8436be4500b476";
         this.weatherApiKey = "bbeb94a0d34c88951a762a3585cf1b1c";
+        this.timers = [];
     }
     save() {
         localStorage.sayWeatherUserData = JSON.stringify(sayWeatherUserData);
